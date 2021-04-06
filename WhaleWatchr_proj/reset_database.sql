35ea1469a0fbf4a99a8d668a0b0882ee5a3050b7
Joshua Agron <jagron@db.cse.nd.edu>
2021-04-06 00:53:02 -0400

    updated sql file to function properly

--- WhaleWatchr_proj/reset_database.sql
+++ WhaleWatchr_proj/reset_database.sql
@@ -1,6 +1,7 @@
 drop table student;
-drop table advisor;
 drop table parent;
+drop table advisor;
+
 
 create table advisor(
 	advisor_id int primary key auto_increment,
@@ -26,24 +27,30 @@ create table student(
 	student_id int primary key auto_increment,
 	first_name varchar(20),
 	last_name varchar(20),
-	parent_id int FOREIGN KEY REFERENCES parent(parent_id),
-	advisor_id int FOREIGN KEY REFERENCES advisor(advisor_id), 
+	parent_id int,
+	advisor_id int, 
 	grade varchar(3),
 	birthday date,
 	activity_id int,
-	route_no int
+	route_no int,
+
+	FOREIGN KEY (parent_id)
+	references parent(parent_id),
+
+	FOREIGN KEY (advisor_id)
+	references advisor(advisor_id)
 );
 
 insert into advisor(first_name, last_name, grade)
-	select advisor_first_name, advisor_last_name, grade
+	select distinct advisor_first_name, advisor_last_name, grade
 	from csv_data;
 
 insert into parent(first_name, last_name, phone_number)
-	select parent_first_name, parent_last_name, parent_phone_number
+	select distinct parent_first_name, parent_last_name, parent_phone_number
 	from csv_data;
 
 insert into student(first_name, last_name, parent_id, advisor_id, grade)
-	select csv_data.first_name, csv_data.last_name, parent_id, advisor_id, csv_data.grade
+	select csv_data.student_first_name, csv_data.student_last_name, parent_id, advisor_id, csv_data.grade
 	from csv_data, parent, advisor
 	where csv_data.parent_last_name = parent.last_name
 	and csv_data.parent_first_name = parent.first_name
