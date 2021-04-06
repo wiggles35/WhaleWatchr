aed8dda42f35ec0fdd03456f4181dec77b4a6c99
Gavin Jakubik <gjakubik@nd.edu>
2021-04-05 20:21:37 -0400

    Finalized navigation install

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,8 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
