cbfdb4ef50480c41f4443799b6a25be4f328a619
rwiggles <rwiggles@nd.edu>
2021-05-10 22:25:09 -0400

    syntax fix

--- WhaleWatchr_proj/WhaleWatchr_proj/urls.py
+++ WhaleWatchr_proj/WhaleWatchr_proj/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     re_path(r'api/parents/$', views.parents_list),
     re_path(r'api/parents/([0-9]+)$', views.parents_detail),
     re_path(r'api/updateRequest/$', views.updateRequest_list),
-    re_path(r'api/updateRequest/([0-9])+)$', views.updateRequest_detail),
+    re_path(r'api/updateRequest/([0-9]+)$', views.updateRequest_detail),
     re_path(r'api/activityChange/$', views.activityChange_list),
     re_path(r'api/activityDetail/$', views.activityDetail_list),
     re_path(r'api/activityDetail/([0-9]+)$', views.activityDetail_detail),
