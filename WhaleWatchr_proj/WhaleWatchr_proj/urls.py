02589dc423546f4f6b7f3a300dab1221fa56f4f1
rwiggles <rwiggles@nd.edu>
2021-05-17 02:27:11 -0400

    update

--- WhaleWatchr_proj/WhaleWatchr_proj/urls.py
+++ WhaleWatchr_proj/WhaleWatchr_proj/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/admin/$', views.admin_list),
     re_path(r'^api/students/$', views.students_list),
     re_path(r'^api/students/([0-9]+)$', views.students_detail),
     re_path(r'^api/advisors/$', views.advisors_list),
